17face67874ece35a9cd463e084e3de2e6299117
DYM <re@dym.sh>
2017-10-29 17:46:46 +0100

    unneccesary separate cert for www-subdomain?
    - `cat` to the point
    
    - separate cert for `www.` subdomain, why?
      eps if you set `www CNAME @` in dns settings

--- renew.sh
+++ renew.sh
@@ -3,7 +3,7 @@ set -e
 
 # begin configuration
 
-domains=( nerdz.eu www.nerdz.eu )
+domains=( nerdz.eu )
 email=nessuno@nerdz.eu
 w_root=/home/nessuno/
 user=nessuno
@@ -18,10 +18,13 @@ fi
 
 
 for domain in "${domains[@]}"; do
-    /usr/bin/certbot certonly --agree-tos --renew-by-default --email $email --webroot -w $w_root$domain -d $domain
-    cat /etc/letsencrypt/live/$domain/privkey.pem  /etc/letsencrypt/live/$domain/cert.pem > ssl.pem
-    cp ssl.pem /etc/lighttpd/$domain.pem
-    cp /etc/letsencrypt/live/$domain/fullchain.pem /etc/lighttpd/
+    /usr/bin/certbot certonly --agree-tos --renew-by-default \
+        --email $email --webroot -w $w_root$domain \
+        -d $domain -d www.$domain
+    cat /etc/letsencrypt/live/$domain/privkey.pem \
+        /etc/letsencrypt/live/$domain/cert.pem \
+        > /etc/lighttpd/$domain.pem
+    cp /etc/letsencrypt/live/$domain/fullchain.pem \
+       /etc/lighttpd/
     chown -R $user:$group /etc/lighttpd/
-    rm ssl.pem
 done
